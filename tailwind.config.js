4295326ac45f100d8330455aba40a3c02a439103
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-08-12 20:51:28 -0300

    feat: add colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        league: ['League Spartan', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        'blue-primary': '#5463FF',
+        'blue-secondary': '#090CE8',
+        'blue-tertiary': '#1011A6',
+        yellow: '#D1DC35',
+        'grey-primary': '#999999',
+        'grey-secondary': '#656565',
+        'white-primary': '#cccccc'
+      }
+    }
   },
   plugins: []
 }
