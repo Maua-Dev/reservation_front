f605ca8989b865c4ec0836074550f41e0239a152
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-08-12 21:25:57 -0300

    feat: add login button

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,14 +2,14 @@ import { Button } from './buttont'
 
 export function Navbar() {
   return (
-    <nav className="flex select-none items-center justify-between px-6 py-5 font-league">
+    <nav className="flex select-none items-center justify-between px-10 py-5 font-league">
       <h1 className="">LOGO</h1>
       <div className="flex gap-24 text-2xl font-medium text-blue-primary">
         <p className="cursor-pointer">INÍCIO</p>
         <p className="cursor-pointer">MENU DE RESERVAS</p>
         <p className="cursor-pointer">SOBRE NÓS</p>
       </div>
-      <Button>LOGIN</Button>
+      <Button>Login</Button>
     </nav>
   )
 }
