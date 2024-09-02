84b4301de7bddaaccf9c3b1ef70a5788b24152f9
Flavio Murata <flaviomurata02@gmail.com>
2024-09-02 11:04:09 -0300

    fix(home-dev): fix mobile responsiveness

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -9,7 +9,7 @@ export function Home() {
         </h1>
 
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row">
-          <div className="text-md flex flex-col font-poppins text-white lg:text-xl">
+          <div className="text-md order-2 flex flex-col items-center font-poppins text-white md:order-1 md:items-start lg:text-xl">
             <p className="">
               O Dev. Community Mauá, fundado em 2021, é uma extracurricular do
               Instituto Mauá de Tecnologia e tem por objetivo o desenvolvimento
@@ -28,7 +28,7 @@ export function Home() {
           <img
             src="https://s3-alpha-sig.figma.com/img/f5b0/08ad/e722be717a10260acfe38c11437d6f60?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VodEb~kZug6myIj-uY3HDyCSk3A9mFvqRV8clIljRrkMLRZBN98QYJ7PYxKoZsdA9-K~-auHsUdjeh-9VWjlB0mi9mpcPEzsiP7hWqD1vlWTWISrN~903PaiqG5gyQ46f8Z3fgERKzFjxshzio9oGS5SZgUZU35Ps9XBU2QfJcpA0ykHwbMPtZ6ltVBJaOFjHMe3FRC~GfikjzhBMzp~~XWgRO8BqVFpE7Vq4K4obOft0uJK3fBq4u3WIIquQNydfUHbj~OXbtRU7j-pE--U52dZOd1W3gIMx4dJ85gEAg6MyiTwgzwbXl~UaZkNyol9Qr3kuw0e4fJwpm2hUhWPCg__"
             alt=""
-            className="h-auto w-full max-w-sm rounded-md border-2 border-white md:max-w-sm lg:max-w-xl"
+            className="order-1 h-auto w-full max-w-sm rounded-md border-2 border-white md:order-2 md:max-w-sm lg:max-w-lg"
           />
         </div>
       </section>
