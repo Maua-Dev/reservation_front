b2e719dcd978eec60fc699d6fbea528f5adaca9d
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-09-02 21:36:01 -0300

    refactor: change var name

--- src/@tests/application/usecase/get-user-list-usecase.test.ts
+++ src/@tests/application/usecase/get-user-list-usecase.test.ts
@@ -5,8 +5,8 @@ import { GetUserListUseCase } from '@/application/usecase/get-user-list-usecase'
 describe('get user list use case', () => {
   it('should return a list of users', async () => {
     const userRepository = new UserRepositoryMock()
-    const getUserUseCase = new GetUserListUseCase(userRepository)
-    const users = await getUserUseCase.execute()
+    const getUserListUseCase = new GetUserListUseCase(userRepository)
+    const users = await getUserListUseCase.execute()
 
     expect(users).toHaveLength(6)
   })
