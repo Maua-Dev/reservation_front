6bdb7527f5146905d39b65b7a6fe8dc763fea0e6
Flavio Murata <flaviomurata02@gmail.com>
2024-09-05 10:25:14 -0300

    feat(home-carousel): add base carousel with swipe animation

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -1,19 +1,44 @@
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+import { useState } from 'react'
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from 'react-icons/fa'
+import { FaCircleArrowLeft, FaCircleArrowRight } from 'react-icons/fa6'
 
 type Props = {
-  children: React.ReactNode
+  children: React.ReactNode[]
 }
 
 export default function Carousel({ children: slides }: Props) {
+  const [curr, setCurr] = useState(0)
+
+  const next = () =>
+    setCurr((curr) =>
+      curr === 0 ? curr + 1 : curr < slides.length - 1 ? curr + 1 : curr
+    )
+
+  const prev = () =>
+    setCurr((curr) =>
+      curr === slides.length - 1 ? curr - 1 : curr > 0 ? curr - 1 : curr
+    )
+
   return (
-    <div className="relative overflow-hidden">
-      <div className="flex">{slides}</div>
+    <div className="relative w-full overflow-hidden">
+      <div
+        className="flex transition-transform duration-500 ease-out"
+        style={{ transform: `translateX(-${curr * 100}%)` }}
+      >
+        {slides}
+      </div>
       <div className="absolute inset-0 flex items-center justify-between p-4">
-        <button className="rounded-full bg-white/80 p-1 text-gray-800 shadow hover:bg-white">
-          <IoIosArrowBack size={40} />
+        <button
+          onClick={prev}
+          className="rounded-full p-1 text-white/80 shadow hover:text-white"
+        >
+          <FaCircleArrowLeft size={50} />
         </button>
-        <button className="rounded-full bg-white/80 p-1 text-gray-800 shadow hover:bg-white">
-          <IoIosArrowForward size={40} />
+        <button
+          onClick={next}
+          className="rounded-full p-1 text-white/80 shadow hover:text-white"
+        >
+          <FaCircleArrowRight size={50} />
         </button>
       </div>
     </div>
