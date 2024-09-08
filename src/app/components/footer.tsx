46502390ffbeb2d16af26cfe2939128188b22124
Flavio Murata <flaviomurata02@gmail.com>
2024-09-08 18:32:40 -0300

    feat(home-dev): add footer to bottom of the screen

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="flex w-full bg-white py-4 md:py-8">
+      <div className="flex w-full flex-col items-center justify-center">
+        <p className="font-poppins text-2xl text-blue-primary">
+          Reservation Mauá - 2024
+        </p>
+      </div>
+    </footer>
+  )
+}
--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,13 +1,15 @@
 import { Button } from '@/app/components/button'
 import Carousel from '@/app/components/carousel'
+import Footer from '@/app/components/footer'
 import { HomeDev } from '@/app/components/home-dev'
 
 export function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-      <section className="flex w-full flex-col items-center bg-blue-primary py-20">
+      <section className="flex w-full flex-grow flex-col items-center justify-center bg-blue-primary pb-4">
         <HomeDev />
       </section>
+      <Footer />
     </main>
   )
 }
