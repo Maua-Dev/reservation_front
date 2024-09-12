89cc825c13886682026c1c3369f01d8415c7f7c8
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-09-11 22:51:10 -0300

    refactor: remove margins and paddings

--- src/app/components/home-about.tsx
+++ src/app/components/home-about.tsx
@@ -16,9 +16,9 @@ export function About() {
   }, [])
 
   return (
-    <div className="mx-auto flex h-screen w-full max-w-6xl flex-col items-start justify-start bg-blue-primary md:p-4">
-      <div className="flex w-full max-w-[1280px] flex-col items-center justify-between md:flex-row-reverse md:py-12 lg:py-0">
-        <div className="mt-16 flex-none sm:mt-16 sm:w-full sm:py-2 md:mt-32 md:w-2/5 md:px-2 md:py-2 lg:mr-12 lg:mt-36 lg:w-5/12">
+    <div className="flex h-screen w-full flex-col items-start justify-start bg-blue-primary pt-16 md:items-center md:justify-center md:pt-24 xl:pt-0">
+      <div className="flex w-full max-w-[1280px] flex-col items-center justify-center gap-6 px-0 md:flex-col-reverse md:gap-16 md:px-24 xl:flex-row-reverse">
+        <div className="flex flex-none flex-col sm:w-full md:w-4/5 lg:w-3/5 xl:w-5/12">
           <div className="pointer-events-auto relative">
             <Carousel onSlideChange={handleSlideChange}>
               {slides.map((s) => (
@@ -35,17 +35,17 @@ export function About() {
             ))}
           </div>
         </div>
-        <div className="flex flex-col text-center sm:text-center md:justify-start md:text-start lg:items-start lg:text-start">
-          <h1 className="hidden px-6 font-league font-normal text-white md:mt-40 md:flex md:text-4xl lg:mt-56 lg:text-5xl">
+        <div className="flex flex-col gap-8 px-6 text-center sm:text-center md:justify-start md:px-0 md:text-start">
+          <h1 className="text-center font-league text-3xl text-white sm:text-4xl lg:text-start lg:text-5xl">
             Reservation Mauá
           </h1>
-          <p className="mt-4 px-5 font-poppins text-xl font-light text-white sm:px-12 sm:text-2xl md:mr-12 md:px-6 md:text-justify md:text-xl lg:mr-28 lg:mt-8 lg:text-2xl">
+          <p className="font-poppins text-lg font-light text-white sm:text-xl md:text-justify lg:text-2xl">
             O Reservation Mauá é o sistema de reservas de salas do Instituto
             Mauá de Tecnologia, projetado para facilitar o agendamento de
             espaços para estudos, reuniões e eventos acadêmicos.
           </p>
-          <div className="mt-4 flex items-center justify-center sm:mt-8 sm:items-center sm:justify-center md:mt-2 md:items-start md:justify-start md:px-6 lg:mt-8">
-            <Button className="w-60 text-xl font-semibold sm:w-52 sm:p-2 sm:text-xl md:mt-8 md:w-48 md:p-2 md:text-xl lg:w-52 lg:px-0 lg:text-xl">
+          <div className="flex items-center justify-center lg:items-start lg:justify-start">
+            <Button className="px-4 text-lg font-semibold sm:text-xl">
               Menu de Reservas
             </Button>
           </div>
