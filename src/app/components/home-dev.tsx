a8b817dcfe9ed031be6e0a1b22a9585f9c67570c
Flavio Murata <flaviomurata02@gmail.com>
2024-09-11 22:05:32 -0300

    style(home-dev): add images to assets folder

--- src/app/components/home-dev.tsx
+++ src/app/components/home-dev.tsx
@@ -1,12 +1,10 @@
 import { Button } from '@/app/components/button'
-import Carousel from '@/app/components/carousel'
+import imageDev1 from '@/app/assets/image-1.svg'
+import imageDev2 from '@/app/assets/image-2.svg'
+import imageDev3 from '@/app/assets/image-3.svg'
 
 export function HomeDev() {
-  const slides = [
-    'https://s3-alpha-sig.figma.com/img/14da/ffee/22783026fda0150a0ccc9af578814ff5?Expires=1726444800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TmXt3lHmDzr4e4RB1lxlUDqTDdk-4BxpE9MhlJH8icyjZohh~54rdeuuSJnFDeHQTV~lbKeOEyqszJ1m0P9-o79rb6Cye~ReOtc22C3QdmmvRC-~5Zl4Rz4m58~ejyfBPefQXhyVRfElUyZx-plJiVTsF4imRQaqzOYEJPphvjNarLLJMbyJmwSSQDJSNhGZ~FjAK3WN5whQnuzP-Bf84aB1r-wpkbPok4h9dz9Bz2NaTCMnB8KZQPqr8-7OHqRk3QnzQSz6puyEqsAuZiCYxSCj6Z3bp~Mt6FudoFQpvB33yH~yUIuPdi37flddiq4EsMYQkDNvyfj7ZnYHWJvX2g__',
-    'https://s3-alpha-sig.figma.com/img/4ee7/d195/a4278338a326759c9a10d62d0760a0b2?Expires=1726444800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=F~rZUha-Hp1-rtnx4ffasIFdVq5uDSrNSwfaiJ-O-pZmIUb5yO06Fqom3i4-NRpdjcUCkbVlq3PJIOaeu13zVjXttV~3IZgtnh808D0jdvGHBl~OQnr6Agx~QIQZa~SFoY7fxRUxWJoohEDXR1Cb6zj1H-TLOkgFCKoKJTUPUcaM6gXj0dNHN5UITFDsLn6F0XwOSoqWUf53Ej~8wbXyW2VhSo0c7Cd5HNHD7tDqobU5ZrRjT3yfmjIRAOb9RAGEyA-he7l7WjoZ-tJmlEIcCfd97qanVy8-Tu6W5s3xTCY761QSdn8kCyp9r44EIIDSmwQ8QxlzfU4~GfQRfCF9vA__',
-    'https://s3-alpha-sig.figma.com/img/e799/0061/f827f9bb87eff13035b8e733214febec?Expires=1726444800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=c8AScVI~2dzcTNd1a552nn68TWYAS-W68Ih9icuZx6WEmAFPMKvZQhgnYRZs4Y5nNudVYwEtNWSQvdOwyph5kVnl2ZLfj0nb~7ELDV75nf5U0fTFIdV9FlMblDVo2y73TWhW~t~nutnRIfb4NwH2m1ry8-C8mFuDCJZNCOPq06ys2uwBq3pq0Ta8eIDMV0jFnh05zIhyU3qSkEcxWXxYglsEwmjiriIwbOxBnHKb7D8TbUqA9IgonB7M8gA-~xX-r3E53HIOldDh65SyXTorupBVUof-xoAEVROP6P6~5OSVkbD5JHYpGtsrk2BRVq2QOPXk6nrWomiXHmbjbijB4w__'
-  ]
+  const slides = [imageDev1, imageDev2, imageDev3]
 
   return (
     <>
@@ -31,13 +29,13 @@ export function HomeDev() {
             Acesse nosso portfolio aqui!
           </Button>
         </div>
-        <div className="order-1 flex w-full justify-center md:order-2 md:w-1/2">
-          <Carousel>
-            {slides.map((s) => (
-              <img key={s} src={s} />
-            ))}
-          </Carousel>
-        </div>
+        {/* <div className="order-1 flex w-full justify-center md:order-2 md:w-1/2"> */}
+        {/*   <Carousel> */}
+        {/*     {slides.map((s) => ( */}
+        {/*       <img key={s} src={s} /> */}
+        {/*     ))} */}
+        {/*   </Carousel> */}
+        {/* </div> */}
       </div>
     </>
   )
