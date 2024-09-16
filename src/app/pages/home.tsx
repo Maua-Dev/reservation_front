abe8812f7cc13ec4581138009e8f952854171939
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-09-16 20:01:21 -0300

    feat: add reservations component to home page

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,17 +1,14 @@
 import Footer from '@/app/components/footer'
 import { Dev } from '@/app/components/dev'
 import { About } from '../components/about'
+import { Reservations } from '../components/reservations'
 
 export function Home() {
   return (
     <main className="flex h-auto flex-col items-center justify-center">
-      {/* <section className="w-full bg-blue-primary pb-16 xl:py-0"> */}
       <About />
-      {/* </section> */}
-
-      {/* <section className="flex w-full flex-grow flex-col items-center justify-center bg-blue-primary pb-8"> */}
+      <Reservations />
       <Dev />
-      {/* </section> */}
       <Footer />
     </main>
   )
