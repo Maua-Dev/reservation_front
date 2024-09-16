6d03efed91dd226d065e8f491fd2dc3f70896696
Flavio Murata <flaviomurata02@gmail.com>
2024-09-16 13:29:25 -0300

    feat(home-about): add section element to about component

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -4,9 +4,12 @@ import { About } from '../components/home-about'
 
 export function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center bg-green-100">
-      <About/>
-      <section className="flex w-full flex-grow flex-col items-center justify-center bg-blue-primary pb-4">
+    <main className="flex h-auto flex-col items-center justify-center">
+      <section className="w-full bg-blue-primary pb-16 xl:py-0">
+        <About />
+      </section>
+
+      <section className="flex w-full flex-grow flex-col items-center justify-center bg-blue-primary pb-8">
         <HomeDev />
       </section>
       <Footer />
