09e890ac4442349babf11c8e4675e58fc14b200e
Flavio Murata <flaviomurata02@gmail.com>
2024-10-03 21:37:26 -0300

    feat(reserve): add modal component

---
+++ src/app/components/modal.tsx
@@ -0,0 +1,29 @@
+import { IoClose } from 'react-icons/io5'
+
+type ModalProps = {
+  open: boolean
+  onClose: () => void
+  children: React.ReactNode
+}
+
+export function Modal({ open, onClose, children }: ModalProps) {
+  return (
+    <div
+      onClick={onClose}
+      className={`fixed inset-0 flex items-center justify-center transition-colors ${open ? 'visible bg-black/20' : 'invisible'}`}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={`rounded-xl bg-white p-6 shadow transition-all ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'} `}
+      >
+        <button
+          onClick={onClose}
+          className="absolute right-2 top-2 rounded-lg bg-white p-1 text-gray-400 hover:bg-gray-50 hover:text-gray-600"
+        >
+          <IoClose size={30} />
+        </button>
+        {children}
+      </div>
+    </div>
+  )
+}
