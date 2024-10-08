6a20b290c1f8420664d7ac32f4c49dda26c5cd33
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-10-08 20:19:16 -0300

    reafctor: refat update and delete params

--- src/domain/interfaces/court-repository-interface.ts
+++ src/domain/interfaces/court-repository-interface.ts
@@ -1,20 +1,30 @@
 import { STATUS } from '@/utils/enums/status'
 import { Court } from '../entities/court'
 
+type updateCourtParams = {
+  newStatus?: STATUS
+  newPhoto?: string
+}
+
+type createCourtParams = {
+  number: number
+  status: STATUS
+  isField: boolean
+  photo?: string
+}
+
 export interface CourtRepositoryInterface {
   getCourt(number: number): Promise<Court>
   getAllCourts(): Promise<Court[]>
-  createCourt(
-    number: number,
-    status: STATUS,
-    isField: boolean,
-    photo?: string
-  ): Promise<Court>
+  createCourt({
+    number,
+    status,
+    isField,
+    photo
+  }: createCourtParams): Promise<Court>
   updateCourt(
-    newNumber?: number,
-    newStatus?: STATUS,
-    newIsField?: boolean,
-    newPhoto?: string
+    number: number,
+    { newStatus, newPhoto }: updateCourtParams
   ): Promise<Court>
   deleteCourt(id: number): Promise<Court>
 }
