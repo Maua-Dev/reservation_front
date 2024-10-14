b9baa757e2e639ee68adacbafe1ad812581eaec6
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-10-14 20:49:55 -0300

    refactor: change several css

--- src/app/components/confirm.tsx
+++ src/app/components/confirm.tsx
@@ -7,9 +7,9 @@ type ConfirmProps = {
 
 export function Confirm({ onClose }: ConfirmProps) {
   return (
-    <div className="flex h-40 w-64 flex-col rounded-xl bg-white px-2 py-4 md:h-[319px] md:w-[805px]">
-      <div className="flex flex-grow items-center justify-center">
-        <p className="mb-4 text-center font-poppins text-sm font-medium text-black md:text-3xl">
+    <div className="flex h-40 w-full flex-col items-center justify-center gap-4 rounded-xl bg-white px-0 pt-4 md:h-full md:w-full">
+      <div className="flex flex-grow items-center justify-center px-8 py-2">
+        <p className="text-center font-poppins text-sm font-medium text-black md:text-xl">
           Declaro estar de acordo com as normas internas do CEAF para utilização
           das instalações e materiais esportivos
         </p>
@@ -20,11 +20,11 @@ export function Confirm({ onClose }: ConfirmProps) {
             onClose()
             e.preventDefault()
           }}
-          className="flex h-10 flex-1 items-center justify-center rounded-l bg-red-600 text-white"
+          className="flex h-10 flex-1 items-center justify-center rounded-bl bg-red-600 text-white"
         >
           <IoClose className="text-3xl" />
         </button>
-        <button className="flex h-10 flex-1 items-center justify-center rounded-r bg-green-700 text-white">
+        <button className="flex h-10 flex-1 items-center justify-center rounded-br bg-green-700 text-white">
           <IoMdCheckmark className="text-3xl" />
         </button>
       </div>
--- src/app/components/modal.tsx
+++ src/app/components/modal.tsx
@@ -1,28 +1,17 @@
-import { IoClose } from 'react-icons/io5'
-
 type ModalProps = {
   open: boolean
   onClose: () => void
   children: React.ReactNode
 }
 
-export function Modal({ open, onClose, children }: ModalProps) {
+export function Modal({ open, children }: ModalProps) {
   return (
     <div
       className={`fixed inset-0 flex items-center justify-center transition-colors ${open ? 'visible z-20 bg-black/20' : 'invisible'}`}
     >
       <div
-        className={`rounded-xl bg-white w-[1000px] p-6 shadow transition-all ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'} `}
+        className={`w-[70%] rounded-xl bg-white shadow transition-all ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'} `}
       >
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            onClose()
-          }}
-          className="absolute right-2 top-2 rounded-lg bg-white p-1 text-gray-400 hover:bg-gray-50 hover:text-gray-600"
-        >
-          <IoClose size={30} />
-        </button>
         {children}
       </div>
     </div>
