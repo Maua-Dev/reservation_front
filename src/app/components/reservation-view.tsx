40397f0bf6a07caa00ddbd58ce61a444d8e91fda
Giulia Soares <giulialeticiasoares.com>
2024-12-19 17:19:24 -0300

    change the view to become recycle component

---
+++ src/app/components/reservation-view.tsx
@@ -0,0 +1,42 @@
+import { Button } from './button'
+
+interface ReservationCardProps {
+  date: string
+  time: string
+  court: string
+  status: string
+  onCancel: () => void
+}
+
+export function ReservationCard({
+  date,
+  time,
+  court,
+  status,
+  onCancel
+}: ReservationCardProps) {
+  return (
+    <div className="flex h-36 w-full cursor-pointer flex-row items-start rounded-lg bg-blue-primary p-4 md:h-44">
+      <div className="flex flex-col gap-1">
+        <p className="text-xl font-bold text-white md:text-2xl">{date}</p>
+        <p className="text-xs font-normal text-white sm:text-base md:text-lg">
+          Hora: {time}
+        </p>
+        <p className="text-xs font-normal text-white sm:text-base md:text-lg">
+          Quadra: {court}
+        </p>
+        <p className="text-xs font-normal text-white sm:text-base md:text-lg">
+          Status: {status}
+        </p>
+      </div>
+      <div className="ml-auto flex flex-col justify-end">
+        <Button
+          className="h-8 w-20 bg-white p-2 text-sm font-bold text-black transition-colors hover:bg-red-400 md:w-36 lg:h-10 lg:text-lg"
+          onClick={onCancel}
+        >
+          Cancelar
+        </Button>
+      </div>
+    </div>
+  )
+}
