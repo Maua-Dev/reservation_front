82f87e1a90b5946e8fa70c7ed6fbb54e2b37715a
tokujiTO <tiagomassuda123@gmail.com>
2025-04-20 13:24:54 -0300

    feat: start admin reserve screen

--- src/app/pages/admin/adminReserve.tsx
+++ src/app/pages/admin/adminReserve.tsx
@@ -1,16 +1,8 @@
-
 export default function AdminReserve() {
   return (
-    <main className="flex h-auto flex-col items-center justify-center">
-      <div className="flex h-[80vh] w-full flex-col items-center justify-center bg-gray-50">
-        <h1 className="font-league text-[50px] font-bold text-blue-primary">
-          Olá Administrador!
-        </h1>
-        <p className="mt-4 w-3/4 text-center font-poppins text-2xl text-blue-primary">
-          O Reservation Mauá é o sistema de reserva de salas, auditórios e
-          quadras do Instituto Mauá de Tecnologia, projetado para facilitar o
-          agendamento de espaços para estudos, reuniões e eventos acadêmicos.
-        </p>
+    <main className="flex h-auto flex-col items-center justify-center pt-24">
+      <div className="flex h-[8vh] w-full grid-cols-8 gap-4 bg-blue-primary p-4 md:p-8">
+        <p>Janeiro</p>
       </div>
     </main>
   )
