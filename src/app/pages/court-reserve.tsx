459b9f9bf9cf948178ae682eb6ed314c2d7eb48b
tokujiTO <tiagomassuda123@gmail.com>
2025-04-25 16:58:11 -0300

    feat: add isField prop in Court component

--- src/app/pages/court-reserve.tsx
+++ src/app/pages/court-reserve.tsx
@@ -1,27 +1,9 @@
-import { Modal } from "@/app/components/modal"
-import { useState } from "react"
-import { Button } from "@/app/components/button"
-import { ReservationDetails } from "../components/reservation-details"
 import { Court } from "../components/court-calendary"
 
 export function CourtReserve() {
-  const [openModal, setOpenModal] = useState<string | null>(null)
-  const modalities = ["Basquete", "Handbol", "Futsal", "Vôlei", "Tênis"]
-  const equipments = [
-    "Bola de futsal",
-    "Bola de handol",
-    "Bola de tênis",
-    "Bola de vôlei",
-    "Raquete de tênis"
-  ]
-  const options = ["Quadra 1", "Quadra 2", "Quadra 3"]
-  const isChecked = [true, false]
-
-  const closeModal = () => setOpenModal(null)
-
   return (
     <main className='flex min-h-screen items-center justify-center gap-6 bg-gray-50 pt-24'>
-      <Court />
+      <Court isField={true} />
     </main>
   )
 }
