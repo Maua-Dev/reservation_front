e602e8ca003c5e9887bd62dd72e70947a6ef468e
tokujiTO <tiagomassuda123@gmail.com>
2025-04-25 16:48:50 -0300

    feat: add court-reserve page

--- src/app/pages/court-reserve.tsx
+++ src/app/pages/court-reserve.tsx
@@ -1,47 +1,27 @@
-import { Modal } from '@/app/components/modal'
-import { useState } from 'react'
-import { Button } from '@/app/components/button'
-import { ReservationDetails } from '../components/reservation-details'
+import { Modal } from "@/app/components/modal"
+import { useState } from "react"
+import { Button } from "@/app/components/button"
+import { ReservationDetails } from "../components/reservation-details"
+import { Court } from "../components/court-calendary"
 
 export function CourtReserve() {
   const [openModal, setOpenModal] = useState<string | null>(null)
-  const modalities = ['Basquete', 'Handbol', 'Futsal', 'Vôlei', 'Tênis']
+  const modalities = ["Basquete", "Handbol", "Futsal", "Vôlei", "Tênis"]
   const equipments = [
-    'Bola de futsal',
-    'Bola de handol',
-    'Bola de tênis',
-    'Bola de vôlei',
-    'Raquete de tênis'
+    "Bola de futsal",
+    "Bola de handol",
+    "Bola de tênis",
+    "Bola de vôlei",
+    "Raquete de tênis"
   ]
-  const options = ['Quadra 1', 'Quadra 2', 'Quadra 3']
+  const options = ["Quadra 1", "Quadra 2", "Quadra 3"]
   const isChecked = [true, false]
 
   const closeModal = () => setOpenModal(null)
 
   return (
-    <main className="flex min-h-screen items-center justify-center gap-6 bg-gray-50">
-      <Button onClick={() => setOpenModal('first')}>Reserva</Button>
-      <Button onClick={() => setOpenModal('second')}>Histórico</Button>
-
-      {/* <Modal open={openModal === 'first'} onClose={closeModal}>
-        <Form
-          modalities={modalities}
-          equipments={equipments}
-          options={options}
-          onClose={closeModal}
-        />
-      </Modal> */}
-
-      <Modal open={openModal === 'second'} onClose={closeModal}>
-        <ReservationDetails
-          location={options[0]}
-          modality={modalities[1]}
-          equipments={equipments}
-          time={10}
-          isChecked={isChecked}
-          duration={1}
-        />
-      </Modal>
+    <main className='flex min-h-screen items-center justify-center gap-6 bg-gray-50 pt-24'>
+      <Court />
     </main>
   )
 }
