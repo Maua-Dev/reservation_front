76938aa2b2542d4a784833a405fa39ad9a0cc2a7
Giulia Soares <giulialeticiasoares.com>
2025-04-28 20:07:42 -0300

    change modal

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -82,18 +82,18 @@ export const Form = ({
         e.preventDefault()
       }}
       onClick={(e) => e.stopPropagation()}
-      className="relative flex max-h-[90vh] w-[90%] max-w-[70vw] flex-col gap-4 overflow-y-auto overflow-x-hidden rounded-xl bg-white p-10 tracking-wide text-slate-700 shadow-lg sm:w-[70%]"
+      className="relative flex max-h-[90vh] w-[90%] max-w-[70vw] flex-col gap-5 overflow-x-hidden overflow-y-visible rounded-xl bg-white px-4 py-6 tracking-wide text-slate-700 shadow-lg sm:w-[70%]"
     >
-      <div className="flex flex-col justify-between border-b-2 border-slate-700 pb-4">
+      <div className="flex flex-col justify-between border-b-2 border-slate-700">
         <div className="flex justify-between">
-          <p className="mt-3 font-poppins text-2xl font-bold md:text-3xl">
+          <p className="font-poppins text-2xl font-bold md:text-3xl">
             Daniel Capuzzo
           </p>
-          <p className="mt-3 font-poppins text-2xl font-bold md:text-3xl">
+          <p className="mr-4 font-poppins text-2xl font-bold md:text-3xl">
             22.001122-0
           </p>
           <IoClose
-            className="absolute right-2 top-2 h-8 w-8 cursor-pointer md:h-10 md:w-16"
+            className="absolute left-[95%] top-1 mr-1 h-8 w-8 cursor-pointer px-2 md:h-10 md:w-16"
             onClick={handleClose}
           ></IoClose>
         </div>
@@ -103,27 +103,27 @@ export const Form = ({
         </p>
       </div>
 
-      <div className="flex w-full flex-col justify-start gap-4 font-medium md:flex-row md:items-center">
-        <div className="flex w-40 items-center justify-between rounded p-1 md:w-48">
-          <label className="flex-grow text-center font-poppins text-lg text-white md:text-2xl">
-            <select className="inline-flex items-center justify-center rounded-xl border border-b-4 border-yellow-secondary bg-yellow px-6 py-4">
+      <div className="flex w-full flex-col justify-start font-medium md:flex-row">
+        <div className="flex w-40 items-center justify-between rounded md:w-48">
+          <label className="flex-grow text-center font-poppins text-lg text-white">
+            <select className="inline-flex items-start justify-start rounded-xl border border-b-4 border-yellow-secondary bg-yellow p-2">
               {options.map((option) => (
                 <option key={option}>{option}</option>
               ))}
             </select>
           </label>
         </div>
-        <div className="flex items-center justify-start gap-2 max-md:pt-4">
-          <label className="flex items-center justify-center gap-2 font-poppins text-xl md:text-2xl">
+        <div className="flex items-start justify-start gap-1">
+          <label className="flex items-center justify-center gap-1 font-poppins text-xl">
             <p>Horário</p>
-            <div className="inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-b-4 border-yellow-secondary bg-yellow p-4 text-white">
+            <div className="inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-b-4 border-yellow-secondary bg-yellow p-2 text-white">
               {formatDate(selectedDate.getHours())}:
               {formatDate(selectedDate.getMinutes())}
             </div>
           </label>
           <div className="flex items-center gap-2">
-            <p className="font-poppins text-xl md:text-2xl">Até</p>
-            <label className="inline-flex items-center justify-center rounded-xl border border-b-4 border-yellow-secondary bg-yellow px-6 py-4 font-poppins text-xl text-white md:text-2xl">
+            <p className="font-poppins text-xl">Até</p>
+            <label className="inline-flex items-center justify-center rounded-xl border border-b-4 border-yellow-secondary bg-yellow p-2 font-poppins text-xl text-white">
               {formatDate(selectedDate.getHours() + 1)}:
               {formatDate(selectedDate.getMinutes())}
             </label>
@@ -133,11 +133,11 @@ export const Form = ({
 
       <div className="flex flex-col">
         <div className="flex justify-start">
-          <p className="pt-4 text-left font-poppins text-3xl font-bold">
+          <p className="py-1 text-left font-poppins text-2xl font-bold">
             Modalidade:
           </p>
         </div>
-        <div className="mt-3 flex flex-wrap gap-2 pt-4">
+        <div className="flex flex-wrap gap-2 py-1">
           {modalities.map((modality) => (
             <button
               key={modality}
@@ -146,7 +146,7 @@ export const Form = ({
                 setSelectedModality(modality)
                 setValue('modality', modality)
               }}
-              className={`inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-b-4 p-4 font-poppins text-lg font-medium hover:bg-black/5 active:border-b-2 md:text-xl ${selectedModality === modality ? 'border-yellow bg-yellow/10 text-yellow-secondary hover:bg-yellow/10' : 'text-slate-700'}`}
+              className={`inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-b-4 p-2 font-poppins text-lg font-medium hover:bg-black/5 active:border-b-2 ${selectedModality === modality ? 'border-yellow bg-yellow/10 text-yellow-secondary hover:bg-yellow/10' : 'text-slate-700'}`}
             >
               {modality}
             </button>
@@ -159,12 +159,12 @@ export const Form = ({
 
       <div className="flex flex-col">
         <div className="flex justify-start">
-          <p className="mt-3 text-left font-poppins text-3xl font-bold">
+          <p className="py-2 text-left font-poppins text-2xl font-bold">
             Equipamentos:
           </p>
         </div>
         <div className="flex items-center">
-          <div className="mt-3 flex flex-wrap gap-2 pt-4">
+          <div className="flex flex-wrap gap-2 py-2">
             {equipments.map((equipment) => (
               <button
                 key={equipment}
@@ -173,7 +173,7 @@ export const Form = ({
                   setSelectedEquipment(equipment)
                   setValue('equipment', equipment)
                 }}
-                className={`inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-b-4 p-4 font-poppins text-lg font-medium hover:bg-black/5 active:border-b-2 md:text-xl ${selectedEquipment === equipment ? 'border-yellow bg-yellow/10 text-yellow-secondary hover:bg-yellow/10' : ''}`}
+                className={`inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-b-4 p-2 font-poppins text-lg font-medium hover:bg-black/5 active:border-b-2 ${selectedEquipment === equipment ? 'border-yellow bg-yellow/10 text-yellow-secondary hover:bg-yellow/10' : ''}`}
               >
                 {equipment}
               </button>
@@ -185,30 +185,30 @@ export const Form = ({
         </div>
       </div>
 
-      <div className="mt-3 flex flex-row items-center gap-2">
+      <div className="flex flex-row items-center gap-2">
         <input
           type="checkbox"
           {...register('needsVest')}
-          className="h-10 w-7 border border-slate-500 p-1 md:w-8"
+          className="h-6 w-7 border border-slate-500 p-1 md:w-8"
         />
         <p className="text-md font-poppins font-medium md:text-xl">
           Preciso de colete
         </p>
       </div>
 
-      <div className="mt-3 flex flex-row items-center justify-between">
+      <div className="flex flex-row items-center justify-between">
         <div className="flex items-center gap-2">
           <input
             type="checkbox"
             {...register('shareCourt')}
-            className="h-10 w-7 border border-slate-500 p-1 md:w-8"
+            className="h-6 w-7 border border-slate-500 p-1 md:w-8"
           />
           <p className="text-md font-poppins font-medium md:text-xl">
             Aceito compartilhar quadra
           </p>
         </div>
         <div className="flex w-40 items-center justify-between rounded p-1">
-          <Button className="inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-b-4 border-yellow-secondary px-6 py-4 font-poppins text-lg text-white active:border-b-2 md:text-2xl">
+          <Button className="inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-b-4 border-yellow-secondary px-5 py-2 font-poppins text-lg text-white active:border-b-2">
             Salvar
           </Button>
           <Modal open={open} onClose={() => setOpen(false)}>
