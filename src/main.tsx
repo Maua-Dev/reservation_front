55f46e0627852caf71750247fe833772c3c8de3f
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-05-11 11:33:24 -0300

    feat: add query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,19 @@ import { App } from './app/app.tsx'
 import { UserProvider } from './app/contexts/user-context.tsx'
 import { MsalProvider } from '@azure/msal-react'
 import { msalInstance } from './app/auth/auth-config.ts'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './app/styles/global.css'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </MsalProvider>
+    <QueryClientProvider client={queryClient}>
+      <MsalProvider instance={msalInstance}>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </MsalProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
