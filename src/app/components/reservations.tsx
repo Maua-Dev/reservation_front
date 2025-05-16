d285cfe1066178c8925445d526e724483cb7a90f
Giulia Soares <giulialeticiasoares.com>
2025-05-16 13:46:39 -0300

    fix: change texts on home

--- src/app/components/reservations.tsx
+++ src/app/components/reservations.tsx
@@ -7,14 +7,14 @@ const reservations = [
     image: imgCampo,
     title: 'Campo',
     description:
-      'O Reservation Mauá é o sistema de reserva de salas do Instituto Mauá de Tecnologia, projetado para facilitar o agendamento de espaços para estudos, reuniões e eventos acadêmicos.',
+      'Ambiente aberto, pertencente ao CEAF, onde usuários podem praticar esportes e realizar projetos acadêmicos',
     calendar: '/fieldbeach-reserve'
   },
   {
     image: imgQuadra,
     title: 'Quadra',
     description:
-      'O Reservation Mauá é o sistema de reserva de salas do Instituto Mauá de Tecnologia, projetado para facilitar o agendamento de espaços para estudos, reuniões e eventos acadêmicos.',
+      'Ambiente poliesportivo coberto, pertencente ao CEAF, onde usuários podem praticar esportes e realizar campeonatos',
     calendar: '/court-reserve'
   }
 ]
@@ -22,15 +22,15 @@ const reservations = [
 export function Reservations() {
   return (
     <section id="reservation" className="scroll-smooth">
-      <div className="flex h-auto min-h-screen max-w-7xl flex-col items-center justify-center gap-12 px-6 py-24 md:gap-24 md:px-12">
+      <div className="flex h-auto min-h-screen max-w-7xl flex-col items-center justify-center gap-10 px-6 py-24 md:gap-24 md:px-12">
         <div className="flex flex-col gap-4 text-center">
           <h1 className="font-league text-4xl font-semibold text-blue-primary lg:text-5xl">
             Reserve seu espaço!
           </h1>
-          <p className="hidden font-poppins text-2xl font-normal text-blue-primary lg:flex">
-            O Reservation Mauá é o sistema de reserva de salas, auditórios e
-            quadras do Instituto Mauá de Tecnologia, projetado para facilitar o
-            agendamento de espaços para estudos, reuniões e eventos acadêmicos.
+          <p className="hidden font-poppins text-2xl font-normal text-blue-primary lg:flex lg:flex-col">
+            Sinta-se a vontade para usufruir dos espaços ideais para realizar
+            suas atividades! <span></span> Selecione abaixo o local desejado
+            para realizar a sua reserva:
           </p>
         </div>
         <div className="flex w-full flex-col items-center justify-center gap-6 md:flex-row">
