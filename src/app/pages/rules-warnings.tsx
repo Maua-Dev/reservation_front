93061c0e6d3dbe0ddbb1ed34c53778cb6e83ef1e
Enzo Granado <enzogranado@gmail.com>
2025-08-07 23:48:10 -0300

    feat: add the initial rectangle

--- src/app/pages/rules-warnings.tsx
+++ src/app/pages/rules-warnings.tsx
@@ -1,7 +1,10 @@
 export default function RulesWarnings() {
   return (
     <div className="flex h-[100vh] w-full items-center justify-center bg-red-300">
-      <p className="text-4xl">Rules and warnings here</p>
+      {/* <p className="text-4xl">Rules and warnings here</p> */}
+      <div className="mb-96 flex h-[80px] w-[1050px] items-center justify-center bg-blue-primary text-center text-white">
+        <p>Avisos e Regras</p>
+      </div>
     </div>
   )
 }
