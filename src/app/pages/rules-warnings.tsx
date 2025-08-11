91a785d429c4e97122118534622b5ac4a8436152
Enzo Granado <enzogranado@gmail.com>
2025-08-11 18:06:29 -0300

    fix: fix the size of the blue warning icon

--- src/app/pages/rules-warnings.tsx
+++ src/app/pages/rules-warnings.tsx
@@ -2,9 +2,15 @@ export default function RulesWarnings() {
   return (
     <div className="flex h-[100vh] w-full items-center justify-center bg-red-300">
       {/* <p className="text-4xl">Rules and warnings here</p> */}
-      <div className="mb-96 flex h-[80px] w-[1050px] items-center justify-center bg-blue-primary text-center text-white">
+      <div className="mb-96 flex h-[80px] w-[900px] items-center justify-center bg-blue-primary text-center text-base text-white">
         <p>Avisos e Regras</p>
       </div>
+      {/* <div className="pl-96">
+        <p>Avisos</p>
+      </div>
+      <div>
+        <p>Regras</p>
+      </div> */}
     </div>
   )
 }
