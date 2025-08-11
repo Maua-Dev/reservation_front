d78e6e5cbcc9bbe1c3028eb44dea5bdf679b3bab
Enzo Granado <enzogranado@gmail.com>
2025-08-11 20:43:35 -0300

    feat: add the text "Avisos" and "Regras" to the rules-warnings page

--- src/app/pages/rules-warnings.tsx
+++ src/app/pages/rules-warnings.tsx
@@ -1,16 +1,18 @@
 export default function RulesWarnings() {
   return (
-    <div className="flex h-[100vh] w-full items-center justify-center bg-red-300">
+    <div className="bg-red-30 flex h-[100vh] w-full flex-col items-center justify-start pt-20">
       {/* <p className="text-4xl">Rules and warnings here</p> */}
-      <div className="mb-96 flex h-[80px] w-[900px] items-center justify-center bg-blue-primary text-center text-base text-white">
+      <div className="flex h-[80px] w-[900px] items-center justify-center bg-blue-primary text-center text-base text-white">
         <p>Avisos e Regras</p>
       </div>
-      {/* <div className="pl-96">
-        <p>Avisos</p>
+      <div className="flex h-[10px] w-full items-start justify-between px-96 pl-20 pt-12 text-xl text-blue-700">
+        <div>
+          <p>Avisos</p>
+        </div>
+        <div>
+          <p>Regras</p>
+        </div>
       </div>
-      <div>
-        <p>Regras</p>
-      </div> */}
     </div>
   )
 }
