d946a2d3ba5f15aaf3836554f410028d0256356e
Enzo Granado <enzogranado@gmail.com>
2025-08-13 23:11:40 -0300

    fix: format document

--- src/app/components/rules-warning-card.tsx
+++ src/app/components/rules-warning-card.tsx
@@ -1,15 +1,12 @@
-
 import { useNavigate } from 'react-router-dom'
 type RulesWarningCardProps = {
-    content: string,
+  content: string
 }
-export function RulesWarningCard({
-    content,
-} : RulesWarningCardProps) {
-    const navigate = useNavigate()
-    return(
-        <div className="p-60px flex h-[300px] w-[500px] flex-col items-start justify-start overflow-auto bg-slate-300">
-            <p>{content}</p>
-        </div>
-    )
+export function RulesWarningCard({ content }: RulesWarningCardProps) {
+  const navigate = useNavigate()
+  return (
+    <div className="p-60px flex h-[300px] w-[500px] flex-col items-start justify-start overflow-auto bg-slate-300">
+      <p>{content}</p>
+    </div>
+  )
 }
--- src/app/pages/rules-warnings.tsx
+++ src/app/pages/rules-warnings.tsx
@@ -1,5 +1,5 @@
-import { RulesWarningCard } from "../components/rules-warning-card"
-// import { RulesWarningCard } from 
+import { RulesWarningCard } from '../components/rules-warning-card'
+// import { RulesWarningCard } from
 
 export default function RulesWarnings() {
   return (
@@ -18,9 +18,8 @@ export default function RulesWarnings() {
       </div>
       <div className="flex h-[370px] w-full flex-col items-end justify-between pr-52 pt-12">
         <div>
-        <RulesWarningCard content=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "/>
+          <RulesWarningCard content=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. " />
         </div>
-    
       </div>
     </div>
   )
