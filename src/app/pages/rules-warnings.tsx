334f95c813bdbf186b1ddc267eecb2873ee33288
Enzo Granado <enzogranado@gmail.com>
2025-08-18 15:24:32 -0300

    fix

--- src/app/pages/rules-warnings.tsx
+++ src/app/pages/rules-warnings.tsx
@@ -5,7 +5,7 @@ export default function RulesWarnings() {
   return (
     <div className="bg-red-30 flex h-[100vh] w-full flex-col items-center justify-start pt-20">
       {/* <p className="text-4xl">Rules and warnings here</p> */}
-      <div className="flex h-[80px] w-[900px] items-center justify-center bg-blue-primary text-center text-4xl text-white">
+      <div className="flex h-[80px] w-[900px] items-center justify-center rounded-b-lg bg-blue-primary text-center text-4xl text-white">
         <p>Avisos e Regras</p>
       </div>
       <div className="flex h-[10px] w-full items-start justify-between px-[40rem] pl-20 pt-12 text-2xl text-blue-700">
