9cf2d693a4a66e93fc90d35c939d36f7b7eb4c6d
Giulia Soares <giulialeticiasoares.com>
2025-08-22 10:52:08 -0300

    fix: CD e iac

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -2,8 +2,10 @@ import * as cdk from 'aws-cdk-lib'
 import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
 // import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'
-// import { Certificate } from 'aws-cdk-lib/aws-certificatemanager'
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager'
 import * as iam from 'aws-cdk-lib/aws-iam'
+import * as route53 from 'aws-cdk-lib/aws-route53'
+import * as route53Targets from 'aws-cdk-lib/aws-route53-targets'
 
 import { Construct } from 'constructs'
 
@@ -11,14 +13,9 @@ export class IacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    // dev: sa-east-1
-    // homolog: us-west-2
-    // prod: us-east-1
-    // const stage = process.env.GITHUB_REF_NAME || 'dev'
     const stage = process.env.STAGE || 'dev'
-    // const acmCertificateArn =
-    //   process.env.ACM_CERTIFICATE_ARN ||
-    //   'arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012'
+    const acmCertificateArn = process.env.ACM_CERTIFICATE_ARN || ''
+    const alternativeDomainName = process.env.ALTERNATIVE_DOMAIN_NAME || ''
 
     const s3Bucket = new s3.Bucket(this, 'ReservationFrontBucket' + stage, {
       versioned: true,
@@ -37,35 +34,43 @@ export class IacStack extends cdk.Stack {
       }
     })
 
-    // let viewerCertificate =
-    //   cloudfront.ViewerCertificate.fromCloudFrontDefaultCertificate()
-    // if (stage === 'dev' || stage === 'homolog') {
-    //   viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
-    //     Certificate.fromCertificateArn(
-    //       this,
-    //       'ReservationFrontCertificate-' + stage,
-    //       acmCertificateArn
-    //     ),
-    //     {
-    //       securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021
-    //     }
-    //   )
-    // }
+    if (
+      (stage === 'dev' || stage === 'homolog' || stage === 'prod') &&
+      !acmCertificateArn
+    ) {
+      throw new Error(
+        `ACM_CERTIFICATE_ARN é obrigatório para o stage: ${stage}`
+      )
+    }
 
-    // if (stage === 'prod') {
-    //   viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
-    //     Certificate.fromCertificateArn(
-    //       this,
-    //       'ReservationFrontCertificate-' + stage,
-    //       acmCertificateArn
-    //     ),
-    //     {
-    //       securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021
-    //     }
-    //   )
-    // }
+    // Permite múltiplos domínios alternativos separados por vírgula
+    let domainNames: string[] = []
+    if (alternativeDomainName) {
+      domainNames = alternativeDomainName
+        .split(',')
+        .map((d) => d.trim())
+        .filter(Boolean)
+    }
 
-    const domainName = process.env.DOMAIN_NAME
+    const hostedZoneId = process.env.HOSTED_ZONE_ID || ''
+    const hostedZoneName = process.env.HOSTED_ZONE_NAME || ''
+
+    let viewerCertificate =
+      cloudfront.ViewerCertificate.fromCloudFrontDefaultCertificate()
+
+    if (stage === 'dev' || stage === 'homolog' || stage === 'prod') {
+      viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
+        Certificate.fromCertificateArn(
+          this,
+          'ReservationFrontCertificate-' + stage,
+          acmCertificateArn
+        ),
+        {
+          securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+          aliases: domainNames.length > 0 ? domainNames : undefined
+        }
+      )
+    }
 
     const cloudFrontWebDistribution = new cloudfront.CloudFrontWebDistribution(
       this,
@@ -93,7 +98,7 @@ export class IacStack extends cdk.Stack {
             ]
           }
         ],
-        // viewerCertificate: viewerCertificate,
+        viewerCertificate: viewerCertificate,
         errorConfigurations: [
           {
             errorCode: 403,
@@ -121,6 +126,41 @@ export class IacStack extends cdk.Stack {
         resources: [s3Bucket.arnForObjects('*')]
       })
     )
+
+    if (domainNames.length > 0 && hostedZoneId && hostedZoneName) {
+      const hostedZone = route53.HostedZone.fromHostedZoneAttributes(
+        this,
+        'HostedZone',
+        {
+          hostedZoneId: hostedZoneId,
+          zoneName: hostedZoneName
+        }
+      )
+
+      // Criar um registro para cada domain name alternativo
+      domainNames.forEach((domain, index) => {
+        new route53.ARecord(this, `CloudFrontARecord-${stage}-${index}`, {
+          zone: hostedZone,
+          recordName: domain,
+          target: route53.RecordTarget.fromAlias(
+            new route53Targets.CloudFrontTarget(cloudFrontWebDistribution)
+          )
+        })
+
+        // Criar também registro AAAA para IPv6
+        new route53.AaaaRecord(this, `CloudFrontAAAARecord-${stage}-${index}`, {
+          zone: hostedZone,
+          recordName: domain,
+          target: route53.RecordTarget.fromAlias(
+            new route53Targets.CloudFrontTarget(cloudFrontWebDistribution)
+          )
+        })
+      })
+    } else if (domainNames.length > 0) {
+      console.warn(
+        'Domain names alternativos fornecidos, mas HOSTED_ZONE_ID ou HOSTED_ZONE_NAME não configurados. Registros DNS não serão criados.'
+      )
+    }
 
     new cdk.CfnOutput(this, 'ReservationFrontBucketName-' + stage, {
       value: s3Bucket.bucketName
