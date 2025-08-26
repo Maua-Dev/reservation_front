2bf0a8441f49de0f84fdcc569d78e1ee594fcb28
tokujiTO <tiagomassuda123@gmail.com>
2025-08-26 15:43:41 -0300

    feat: add detect if the user is a Admin

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -2,8 +2,18 @@ import Footer from '@/app/components/footer'
 import { Dev } from '@/app/components/dev'
 import { About } from '../components/about'
 import { Reservations } from '../components/reservations'
+import { useNavigate } from 'react-router-dom'
+import { useUser, useUserQuery } from '../hooks/use-user'
+import { toast } from 'react-toastify'
 
 export function Home() {
+  useUserQuery()
+  const { user } = useUser()
+  const navigate = useNavigate()
+  if (user && user.role === 'ADMIN') {
+    toast.success('Welcome back, Admin!')
+    navigate('/admin-portal')
+  }
   return (
     <main className="flex h-auto flex-col items-center justify-center">
       <About />
