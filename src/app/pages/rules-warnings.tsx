740b7d36803671a1f75110e76a70a2fddaaf087c
Giulia Soares <giulialeticiasoares.com>
2025-08-26 17:46:06 -0300

    fix: branch conflict

--- src/app/pages/rules-warnings.tsx
+++ src/app/pages/rules-warnings.tsx
@@ -3,6 +3,7 @@ import { CardWarnings } from '../components/warnings-card'
 import { Button } from '../components/button'
 import { BsThreeDots } from 'react-icons/bs'
 import { useState, useRef, useEffect } from 'react'
+import EditModal from '../components/edit-modal'
 // import { RulesWarningCard } from
 
 export default function RulesWarnings() {
@@ -118,6 +119,7 @@ export default function RulesWarnings() {
           <div className="flex h-[60vh] w-full flex-col justify-between">
             <RulesWarningCard content=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. " />
           </div>
+          {showModal && <EditModal onClose={() => setShowModal(false)} />}
         </div>
       </div>
     </div>
