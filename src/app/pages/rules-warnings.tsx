275fc89020f85f8c0fc1532825a4942844b4b089
Enzo Granado <enzogranado@gmail.com>
2025-09-02 20:44:40 -0300

    teste

--- src/app/pages/rules-warnings.tsx
+++ src/app/pages/rules-warnings.tsx
@@ -1,28 +1,20 @@
 import { RulesWarningCard } from '../components/rules-warning-card'
 import { CardWarnings } from '../components/warnings-card'
 import { Button } from '../components/button'
-import { useState, useRef, useEffect, useContext } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import EditModal from '../components/edit-modal'
 import NoticeModal from '../components/edit-notice-modal'
-import { alertContext } from '../contexts/alerts-context'
-import { useUser } from '../hooks/use-user'
-import DeleteAlertModal from '../components/admin/delteAlertModal'
 // import { RulesWarningCard } from
 
 export default function RulesWarnings() {
   const [showEdit, setShowEdit] = useState(false)
-  const [showDeleteModal, setShowDeleteModal] = useState(false)
+  // const [showModal, setShowModal] = useState(false)
   const editRef = useRef<HTMLDivElement>(null)
   const [showNoticeModal, setShowNoticeModal] = useState(false)
-  const [selectedAlert, setSelectedAlert] = useState<string | null>(null)
   const [showEditModal, setShowEditModal] = useState(false)
-  const { user } = useUser()
-  const { alerts } = useContext(alertContext)
-  console.log('alerts: ', alerts)
 
   useEffect(() => {
     if (!showEdit && !showNoticeModal && !showEditModal) return
-
     const handleClickOutside = (e: MouseEvent) => {
       if (editRef.current && !editRef.current.contains(e.target as Node)) {
         setShowEdit(false)
@@ -33,57 +25,62 @@ export default function RulesWarnings() {
       document.removeEventListener('mousedown', handleClickOutside)
     }
   }, [showEdit, showEditModal, showNoticeModal])
-
   return (
-    <div className="bg-red-30 flex h-[100vh] w-full flex-col items-center justify-start overflow-hidden bg-white px-14 pt-20">
-      {showDeleteModal && selectedAlert && (
-        <DeleteAlertModal
-          onClose={() => setShowDeleteModal(false)}
-          alertId={selectedAlert}
-        />
-      )}
+    <div className="bg-red-30 flex h-[100vh] w-full flex-col items-center justify-start overflow-y-hidden pt-20">
       <div className="flex h-[66px] w-1/2 items-center justify-center rounded-b-xl bg-blue-primary pt-2 text-center font-poppins text-2xl font-bold text-white">
         <p className="font-bold">AVISOS E REGRAS</p>
       </div>
-      <div className="flex w-full items-center justify-evenly gap-10 overflow-hidden pt-12 text-2xl text-blue-700">
+      <div className="flex w-full items-center justify-evenly gap-10 overflow-scroll px-14 pt-12 text-2xl text-blue-700">
         <div className="flex h-[72vh] w-1/3 flex-col items-center justify-center">
-          <div className="flex w-[100px] flex-row gap-16 pb-5 text-center font-poppins font-bold text-blue-500">
+          <div className="w-[100px] pb-0 text-center font-poppins font-bold text-blue-500">
             Avisos
-            {user?.role === 'ADMIN' && (
-              <div
-                ref={editRef}
-                className="mt-2 flex w-full flex-row justify-end pb-2 pr-8"
-              >
-                <Button
-                  className="flex h-5 w-1/12 items-center justify-center bg-blue-primary font-poppins text-base text-white"
-                  onClick={() => {
-                    setShowEdit(false)
-                    setShowNoticeModal(true)
-                  }}
-                >
-                  +
-                </Button>
-                {showNoticeModal && (
-                  <NoticeModal onClose={() => setShowNoticeModal(false)} />
-                )}
-              </div>
-            )}
           </div>
-
-          <div className="h-[60vh] w-full overflow-x-hidden overflow-y-scroll px-4">
-            <div>
-              {alerts &&
-                alerts.map((alert) => (
-                  <CardWarnings
-                    alert={alert}
-                    key={alert.alert.alert_id}
-                    confirmDelete={() => {
-                      setShowDeleteModal(true)
-                      setSelectedAlert(alert.alert.alert_id)
-                    }}
-                  />
-                ))}
-            </div>
+          <div
+            ref={editRef}
+            className="flex w-full flex-row justify-end pb-4 pr-8"
+          >
+            <Button
+              className="flex h-5 w-1/12 items-center justify-center bg-blue-primary font-poppins text-base text-white"
+              onClick={() => {
+                setShowEdit(false)
+                setShowNoticeModal(true)
+              }}
+            >
+              +
+            </Button>
+            {showNoticeModal && <NoticeModal onClose={() => setShowNoticeModal(false)} />}
+          </div>
+          <div className="h-[60vh] w-full overflow-scroll px-4">
+            <CardWarnings
+              titulo="Quadra 1"
+              data="24/10 à 26/10"
+              conteudo="A quadra estará fechada por causa do evento Eureka bla bla bla bla bla bla bla lba lbbla bla bla bla bla bla bla lba lbbla bla bla bla bla bla bla lba lbbla bla bla bla bla bla bla lba lbbla bla bla bla bla bla bla lba lbbla bla bla bla bla bla bla lba lba bla bla lbabla lbablal bablabl "
+            ></CardWarnings>
+            <CardWarnings
+              titulo="Todas"
+              data="24/10 à 26/10"
+              conteudo="A quadra estará fechada por causa do evento Eureka"
+            ></CardWarnings>
+            <CardWarnings
+              titulo="Todas"
+              data="24/10 à 26/10"
+              conteudo="A quadra estará fechada por causa do evento Eureka"
+            ></CardWarnings>
+            <CardWarnings
+              titulo="Todas"
+              data="24/10 à 26/10"
+              conteudo="A quadra estará fechada por causa do evento Eureka"
+            ></CardWarnings>
+            <CardWarnings
+              titulo="Todas"
+              data="24/10 à 26/10"
+              conteudo="A quadra estará fechada por causa do evento Eureka"
+            ></CardWarnings>
+            <CardWarnings
+              titulo="Todas"
+              data="24/10 à 26/10"
+              conteudo="A quadra estará fechada por causa do evento Eureka"
+            ></CardWarnings>
           </div>
         </div>
 
@@ -91,29 +88,22 @@ export default function RulesWarnings() {
           <div>
             <p className="font-poppins font-bold text-blue-500">Regras</p>
           </div>
-          {user?.role === 'ADMIN' && (
-            <div className="flex w-full flex-col items-end pb-3 pr-8">
-              <Button
-                className="flex h-5 w-1/12 items-center justify-center bg-blue-primary p-3 font-poppins text-base text-white"
-                onClick={() => {
-                  setShowEdit(false)
-                  setShowEditModal(true)
-                }}
-              >
-                Editar
-              </Button>
-              {showEditModal && (
-                <EditModal onClose={() => setShowEditModal(false)} />
-              )}
-            </div>
-          )}
-
+          <div className="flex w-full flex-col items-end pb-3 pr-8">
+            <Button
+              className="flex h-5 w-1/12 items-center justify-center bg-blue-primary p-3 font-poppins text-base text-white"
+              onClick={() => {
+                setShowEdit(false)
+                setShowEditModal(true)
+              }}
+            >
+              Editar
+            </Button>
+            {showEditModal && <EditModal onClose={() => setShowEditModal(false)} />}
+          </div>
           <div className="flex h-[60vh] w-full flex-col justify-between">
             <RulesWarningCard content=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. " />
           </div>
-          {showEditModal && (
-            <EditModal onClose={() => setShowEditModal(false)} />
-          )}
+          {showEditModal && <EditModal onClose={() => setShowEditModal(false)} />}
         </div>
       </div>
     </div>
